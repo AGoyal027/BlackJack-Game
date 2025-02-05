16eac57cacf2af8161bab697ad78e1b3dc7d033f
AGoyal027 <ayushgoyal027@gmail.com>
2025-02-05 23:59:31 +0530

    Added getRandomCard function to generate random card values

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ let cardsEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
 
 playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
